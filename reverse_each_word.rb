4aedc6df17bcf6cfee7cc45b85cbc89076373983
rosie w <rosie.wilt@gmail.com>
2020-04-12 16:54:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,16 @@
 def reverse_each_word(string)
+# new_str = string.split(" ")
+# 
+# new_str.each { |element|
+# element.reverse!
+# }
+# 
+# new_str.join(" ")
 new_str = string.split(" ")
 
-new_str.each { |element|
-element.reverse!
+new_str.collect { |element|
+  element.reverse!
 }
-
 new_str.join(" ")
-
 end 
+
